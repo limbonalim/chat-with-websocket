b8c328878028d88e8254d8efe6c7834c32d9c2f5
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-16 19:33:09 +0600

    added IMessage type for frontend

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,16 @@
 export const BASE_URL = 'http://localhost:8000';
-export const BASE_WS_URL = 'ws://localhost:8000';
+export const BASE_WS_URL = 'ws://localhost:8000/chat';
 
 export enum Roles {
 	user = 'user',
 	moderator = 'moderator',
+}
+
+export enum IMessageType {
+	login = 'LOGIN',
+	logout = 'LOGOUT',
+	newMessage = 'NEW_MESSAGE',
+	sendMessage = 'SEND_MESSAGE',
+	deleteMessage = 'DELETE_MESSAGE',
+	personalMessage = 'SEND_PERSONAL_MESSAGE',
 }
--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,35 +1,56 @@
+import { IMessageType } from './constants.ts';
+
 export interface IMyError {
-  message: string;
+	message: string;
 }
 
 export interface ValidationError {
-  errors: {
-    [key: string]: {
-      name: string;
-      message: string;
-    }
-  },
-  message: string;
-  name: string;
-  _message: string;
+	errors: {
+		[key: string]: {
+			name: string;
+			message: string;
+		};
+	};
+	message: string;
+	name: string;
+	_message: string;
 }
 
 export interface IRegisterForm {
-  email: string;
-  password: string;
-  displayName: string;
+	email: string;
+	password: string;
+	displayName: string;
 }
 
 export interface ILoginForm {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 export interface IUser {
-  _id: string;
-  email: string;
-  displayName: string;
-  avatar?: string;
-  role: string;
-  token: string;
+	_id: string;
+	email: string;
+	displayName: string;
+	avatar?: string;
+	role: string;
+	token: string;
+}
+
+export interface IMessagePayload {
+	_id?: string;
+	user: {
+		displayName: string;
+		avatar: string;
+	};
+	message?: string;
+	createdAt: string;
+	recipient?: {
+		displayName: string;
+		avatar: string;
+	};
+}
+
+export interface IMessage {
+	type: IMessageType;
+	payload: IMessagePayload;
 }
