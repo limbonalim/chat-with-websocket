5e455c7f7b01c1f5e40275566edf9228e81b59c5
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-16 19:35:10 +0600

    added chat in store for frontend

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,7 +1,17 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistStore } from 'redux-persist';
+import {
+	persistReducer,
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistStore,
+} from 'redux-persist';
 import { usersReducers } from '../feachers/Users/usersSlice.ts';
+import { chatReducer } from '../feachers/Chat/chatSlice.ts';
 
 const usersPersistConfig = {
 	key: 'spotify:users',
@@ -10,16 +20,18 @@ const usersPersistConfig = {
 };
 
 const rootReducer = combineReducers({
-	users: persistReducer(usersPersistConfig, usersReducers)
+	chat: chatReducer,
+	users: persistReducer(usersPersistConfig, usersReducers),
 });
 
 export const store = configureStore({
 	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-		serializableCheck: {
-			ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER]
-		}
-	})
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
