7784c1ba6f4df203a498456e8d27e79bd5e2491c
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-18 15:13:40 +0600

    used Protected and change onmessage service

--- frontend/src/components/UI/Protected/Protected.tsx
+++ frontend/src/components/UI/Protected/Protected.tsx
@@ -1,31 +1,27 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../../app/hooks.ts';
-import { selectUser } from '../../../containers/Users/usersSlice.ts';
+import { selectUser } from '../../../feachers/Users/usersSlice.ts';
 import { Roles } from '../../../constants.ts';
 
 interface Props extends PropsWithChildren {
-  isAdmin?: boolean
+	isAdmin?: boolean;
 }
 
-const Protected: React.FC<Props> = ({children, isAdmin}) => {
-  const user = useAppSelector(selectUser);
-  const navigate = useNavigate();
+const Protected: React.FC<Props> = ({ children, isAdmin }) => {
+	const user = useAppSelector(selectUser);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!user) {
-      navigate('/login');
-    }
-    if (isAdmin && user?.role !== Roles.admin) {
-      navigate('/login');
-    }
-  }, [user]);
+	useEffect(() => {
+		if (!user) {
+			navigate('/login');
+		}
+		if (isAdmin && user?.role !== Roles.moderator) {
+			navigate('/login');
+		}
+	}, [user]);
 
-  return (
-    <>
-      {children}
-    </>
-  );
+	return <>{children}</>;
 };
 
 export default Protected;
