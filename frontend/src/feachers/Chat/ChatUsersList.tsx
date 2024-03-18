c145c74bb6ad93cf230cbd19a6ba2417449b3952
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-18 15:16:44 +0600

    show active users in ChatUsersList

--- frontend/src/feachers/Chat/ChatUsersList.tsx
+++ frontend/src/feachers/Chat/ChatUsersList.tsx
@@ -1,13 +1,17 @@
 import { Grid } from '@mui/material';
 import ChatUserItem from '../../components/ChatUserItem/ChatUserItem.tsx';
+import { useAppSelector } from '../../app/hooks.ts';
+import { selectUsers } from './chatSlice.ts';
 
 const ChatUsersList = () => {
+	const users = useAppSelector(selectUsers);
+	const render = users.map(({ displayName, avatar }, index) => (
+		<ChatUserItem key={index} displayName={displayName} avaatar={avatar} />
+	));
+
 	return (
 		<Grid container direction="column" spacing={1}>
-			<ChatUserItem />
-			<ChatUserItem />
-			<ChatUserItem />
-			<ChatUserItem />
+			{render}
 		</Grid>
 	);
 };
