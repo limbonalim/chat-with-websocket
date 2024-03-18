6e709c1b2dbef14970bd817f5d334034322bd954
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-18 15:14:19 +0600

    fixed bugs in ChatMessagesList

--- frontend/src/feachers/Chat/ChatMessagesList.tsx
+++ frontend/src/feachers/Chat/ChatMessagesList.tsx
@@ -6,16 +6,18 @@ import { selectMessages } from './chatSlice.ts';
 const ChatMessagesList = () => {
 	const messages = useAppSelector(selectMessages);
 
-	const render = messages.map(({ payload }) => (
-		<ChatMessageItem
-			key={payload._id}
-			_id={payload._id}
-			message={payload.message}
-			createdAt={payload.createdAt}
-			user={payload.user}
-			recipient={payload.recipient}
-		/>
-	));
+	const render = messages.map(
+		({ _id, message, createdAt, user, recipient }) => (
+			<ChatMessageItem
+				key={_id}
+				_id={_id}
+				message={message}
+				createdAt={createdAt}
+				user={user}
+				recipient={recipient}
+			/>
+		),
+	);
 
 	return (
 		<Grid container direction="column" spacing={1} mb={2}>
