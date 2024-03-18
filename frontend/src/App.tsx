5d52b3ecb474ff217720b67d33632cc5991c0eeb
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-18 15:18:42 +0600

    try navigated to /chat in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import Layout from './components/UI/Layout/Layout.tsx';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import NotFound from './components/UI/Not-Found/NotFound.tsx';
 import Login from './feachers/Users/Login.tsx';
 import Register from './feachers/Users/Register.tsx';
 import Chat from './feachers/Chat/Chat.tsx';
+import { useEffect } from 'react';
 
 const App = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('/chat');
+	}, []);
+
 	return (
 		<>
 			<Layout>
